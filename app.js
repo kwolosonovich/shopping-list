b07e608cc22bbd7b7cdf1498de136c21ef4a5290
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-24 20:22:06 -0700

    added routes

--- app.js
+++ app.js
@@ -8,8 +8,33 @@ app.use(express.json());
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.get("/items", (req, res, next) => {
-    return res.send(items)
+    // question 1 - test route using request.headers
+    console.log(request.headers)
+    debugger
+    return res.json({ items })
 })
+
+app.post("/items", (req, res, next) => {
+    // question 2 - test route using req.body
+    items.push(req.body.name)
+})
+
+app.get("/items/:name", (req, res, next) => {
+    // question 3 - how to access req.body  
+    reqItem = req.params.name
+    res.json( { items: reqItem})
+})
+
+app.patch("/items/:name", (res, req, next) => {
+    // question 3 - how to access req.body  
+    updateItem = req.params.name
+})
+
+app.delete("/items/:delte", (res, req, next) => {
+    deleteItem = req.params.name
+    items.splice(deleteItem)
+});
+
 
 // ** 404 handler */
 app.use(function (req, res, next) {
@@ -17,7 +42,6 @@ app.use(function (req, res, next) {
 });
 
 /** general error handler */
-
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
