694225aec2a434df8076503594b455d6e3ea5d02
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-24 19:18:15 -0700

    added general error handler and 404 handler

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const app = express();
 
 app.use(express.json());
 
+// ** 404 handler */
 
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
 
 module.exports = app;
